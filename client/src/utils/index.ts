d051ee29c93ea4d979a54f3691c089a1238ba1d0
Rahul <rahulsm.1503@gmail.com>
2026-03-29 17:28:45 +0530

    fix: missing util for client side loading states

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,12 +1,24 @@
-export const updateHistory = (company:string,category:string) => {
-    if(localStorage.getItem("user")){
-        const user = JSON.parse(localStorage.getItem("user") as string);
-        if(!user.history){
-            user.history = [];
-        }
-        else{
-            user.history = user.history.push({company,category});
-        }
-        localStorage.setItem("user", JSON.stringify(user));
+export const updateHistory = (company: string, category: string) => {
+  if (localStorage.getItem("user")) {
+    const user = JSON.parse(localStorage.getItem("user") as string);
+    if (!user.history) {
+      user.history = [];
+    } else {
+      user.history = user.history.push({ company, category });
     }
-}
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+};
+
+export const getLoadingTitle = (stage?: string) => {
+  switch (stage) {
+    case "scraping":
+      return "Scraping Products";
+    case "generation":
+      return "Generating Strategies";
+    case "embedding":
+      return "Embedding Products into Vectors";
+    default:
+      return "Loading";
+  }
+};
