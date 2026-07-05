668bfaed63803e004072dbea6fedf9ecaea159d6
Rahul <rahulsm.1503@gmail.com>
2026-07-05 15:20:18 +0530

    fix(client): session lost on reload

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -18,9 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       scope: "openid profile email offline_access",
     }}
     useRefreshTokens={true}
-    cacheLocation={
-      import.meta.env.VITE_NODE_ENV == "development" ? "localstorage" : "memory"
-    }
+    cacheLocation={"localstorage"}
   >
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
